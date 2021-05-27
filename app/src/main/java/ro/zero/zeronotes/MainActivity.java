001e53ea84f5eab27dfe196acd60fbf302d99b1c
Cosma George <georgemagic10@gmail.com>
2021-05-27 16:53:54 +0300

    Finished NavButton.java and document it... mostly. Also added them to activity_main.xml.

--- app/src/main/java/ro/zero/zeronotes/MainActivity.java
+++ app/src/main/java/ro/zero/zeronotes/MainActivity.java
@@ -3,12 +3,34 @@ package ro.zero.zeronotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.sample_nav_button);
+		setContentView(R.layout.activity_main);
+
+		NavButton notesNavButton = findViewById(R.id.navButton_Notes);
+		notesNavButton.select();
+		NavButton notesNavHabits = findViewById(R.id.navButton_Habits);
+		NavButton notesNavCalendar = findViewById(R.id.navButton_Calendar);
+
+		notesNavButton.setOnClickListener(v -> {
+			notesNavButton.select();
+			notesNavHabits.deselect();
+			notesNavCalendar.deselect();
+		});
+		notesNavHabits.setOnClickListener(v -> {
+			notesNavButton.deselect();
+			notesNavHabits.select();
+			notesNavCalendar.deselect();
+		});
+		notesNavCalendar.setOnClickListener(v -> {
+			notesNavButton.deselect();
+			notesNavHabits.deselect();
+			notesNavCalendar.select();
+		});
 	}
 }
--- app/src/main/java/ro/zero/zeronotes/NavButton.java
+++ app/src/main/java/ro/zero/zeronotes/NavButton.java
@@ -6,37 +6,69 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
 /**
- * TODO: document your custom view class.
+ * This UI Component creates a view which has two main components: Text and an Icon. Both are centered vertically
+ * such that the text is aligned to the bottom and the remaining vertical space is assigned to the Icon. <br>
+ * You can use padding to decrease the total size taken up by the active elements (Text, Icon)
+ * without nesting or size reduction.
+ * <br><br><br>
+ * Text Properties:
+ * <ul>
+ * <li><b>navText</b>: The text that will appear under the icon</li>
+ * <li><b>navTextSize</b>: The size of the text.</li>
+ * <li><b>navTextColor</b>: The color of the text.</li>
+ * </ul>
+ * <br>
+ * Icon Properties:
+ * <ul>
+ * <li><b>navIcon</b>: A reference to the icon you want displayed.</li>
+ * <li><b>navIconSize</b>: The size of the icon.</li>
+ * <li><b>navIconOverscale [Optional]</b>: If set to true, the icon will be scaled up to meet the requested size. If set to
+ * false then the icon will scale up to it's native size and no further. Default: false.</li>
+ * </ul>
+ * <br>
+ * Background Properties (All optional):
+ * <ul>
+ * <li><b>selectedColor</b>: The color of the background if <code>.select()</code> is called on this.</li>
+ * <li><b>deselectedColor</b>: The color of the background on startup or if <code>.deselect()</code> is called on this.
+ * If no value is given to this property, it will use the value of <code>android:background</code></li>
+ * </ul>
  */
 public class NavButton extends View {
-//    private String mExampleString;
-//    private int mExampleColor = Color.RED;
-//    private float mExampleDimension = 0;
-//    private Drawable mExampleDrawable;
-//
-//    private TextPaint mTextPaint;
-//    private float mTextWidth;
-//    private float mTextHeight;
 
+	// Text XML Props
 	private String navDestinationText = "None";
 	private int textColor = Color.BLACK;
 	private float textSize = 10;
+	// Icon XML Props
 	private Drawable navIcon;
 	private float iconSize = 10;
 	private boolean overscale = false;
+	// Background XML Props
+	private Integer deselectedBackgroundColor = null;
+	private Integer selectedBackgroundColor = null;
 
-	private int iconHeight = 0;
-	private int iconWidth = 0;
 
+	// Text support vars
 	private TextPaint navTextPaint;
 	private float textWidth;
 	private float textHeight;
+	private final Rect textBounds = new Rect();
+
+	// Icon support vars
+	private int iconHeight = 0;
+	private int iconWidth = 0;
+
+
+	// Button & Background support vars
+	private boolean selected = false;
+	private final int ERROR_COLOR = Color.rgb(255, 0, 255);
 
 	public NavButton(Context context) {
 		super(context);
@@ -56,10 +88,12 @@ public class NavButton extends View {
 		final TypedArray a = getContext().obtainStyledAttributes(
 				attrs, R.styleable.NavButton, defStyle, 0);
 
+		// Get text params
 		navDestinationText = a.getString(R.styleable.NavButton_navText);
 		textSize = a.getDimension(R.styleable.NavButton_navTextSize,textSize);
 		textColor = a.getColor(R.styleable.NavButton_navTextColor, textColor);
 
+		// Get Icon params
         if (a.hasValue(R.styleable.NavButton_navIcon)) {
 			navIcon = a.getDrawable(R.styleable.NavButton_navIcon);
 			navIcon.setCallback(this);
@@ -67,6 +101,17 @@ public class NavButton extends View {
 			iconSize = a.getDimension(R.styleable.NavButton_navIconSize,iconSize);
 			overscale = a.getBoolean(R.styleable.NavButton_navIconOverscale,overscale);
       	}
+        // Get Background params
+		if(a.hasValue(R.styleable.NavButton_deselectedColor)) {
+			deselectedBackgroundColor = a.getColor(R.styleable.NavButton_deselectedColor,ERROR_COLOR);
+		} else {
+			if (getBackground() instanceof ColorDrawable)
+				deselectedBackgroundColor = ((ColorDrawable) getBackground()).getColor();
+		}
+
+		if(a.hasValue(R.styleable.NavButton_selectedColor)) {
+			selectedBackgroundColor = a.getColor(R.styleable.NavButton_selectedColor,ERROR_COLOR);
+		}
 		a.recycle();
 
 		// Set up a default TextPaint object
@@ -82,7 +127,6 @@ public class NavButton extends View {
 		resetText();
 		resetIcon();
 	}
-
 	private void resetText() {
 		navTextPaint.setTextSize(textSize);
 		navTextPaint.setColor(textColor);
@@ -90,7 +134,6 @@ public class NavButton extends View {
 		textWidth = navTextPaint.measureText(navDestinationText);
 		textHeight = navTextPaint.getFontMetrics().bottom;
 	}
-
 	private void resetIcon() {
 		float aspectRatio = (float)navIcon.getIntrinsicWidth() / (float)navIcon.getIntrinsicHeight();
 		iconHeight = (int)iconSize;
@@ -105,7 +148,14 @@ public class NavButton extends View {
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 
-		// TODO: consider storing these as member variables to reduce
+		if(selectedBackgroundColor != null && deselectedBackgroundColor != null) {
+			if(selected) {
+				setBackgroundColor(selectedBackgroundColor);
+			} else {
+				setBackgroundColor(deselectedBackgroundColor);
+			}
+		}
+
 		int paddingLeft = getPaddingLeft();
 		int paddingTop = getPaddingTop();
 		int paddingRight = getPaddingRight();
@@ -125,7 +175,7 @@ public class NavButton extends View {
 					paddingTop + contentHeight - textHeight,
 					navTextPaint);
 
-			Rect textBounds = new Rect();
+			textBounds.setEmpty();
 			navTextPaint.getTextBounds(navDestinationText,0,navDestinationText.length(),textBounds);
 
 			int leftoverHeight = getHeight() - getPaddingTop() - getPaddingBottom() - textBounds.height();
@@ -142,18 +192,14 @@ public class NavButton extends View {
 					paddingTop + (leftoverHeight + finalIconHeight)/2);
 			navIcon.draw(canvas);
 		}
+	}
 
-//		// Draw the text.
-//		canvas.drawText(mExampleString,
-//				paddingLeft + (contentWidth - mTextWidth) / 2,
-//				paddingTop + (contentHeight + mTextHeight) / 2,
-//				mTextPaint);
-//
-//		// Draw the example drawable on top of the text.
-//		if (mExampleDrawable != null) {
-//			mExampleDrawable.setBounds(paddingLeft, paddingTop,
-//					paddingLeft + contentWidth, paddingTop + contentHeight);
-//			mExampleDrawable.draw(canvas);
-//		}
+	public void select() {
+		selected = true;
+		invalidate();
+	}
+	public void deselect() {
+		selected = false;
+		invalidate();
 	}
 }
