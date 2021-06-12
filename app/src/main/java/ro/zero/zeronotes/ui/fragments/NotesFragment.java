820934537ecad62aed6cdf54c0284fad50c52333
Cosma George <georgemagic10@gmail.com>
2021-06-12 23:01:07 +0300

    Implemented date selection.

--- app/src/main/java/ro/zero/zeronotes/ui/fragments/NotesFragment.java
+++ app/src/main/java/ro/zero/zeronotes/ui/fragments/NotesFragment.java
@@ -6,9 +6,14 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import java.time.Clock;
 import java.time.LocalDate;
@@ -20,6 +25,11 @@ import ro.zero.zeronotes.storage.DataStorageManager;
 import ro.zero.zeronotes.ui.NoteRecyclerViewAdapter;
 
 public class NotesFragment extends Fragment {
+
+	private LocalDate selectedDate = LocalDate.now();
+	private LocalDate tempCalendarDate = selectedDate;
+	private RecyclerView recyclerView;
+	private TextView dateTextView;
 
 	public NotesFragment() {
 
@@ -35,7 +45,12 @@ public class NotesFragment extends Fragment {
 							 Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_notes, container, false);
 
-		ArrayList<Note> notes = DataStorageManager.getInstance().saveData.getNotes(LocalDate.now());
+		// Recycler View
+		// ===============
+		recyclerView = view.findViewById(R.id.noteRecycler);
+		recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+		ArrayList<Note> notes = DataStorageManager.getInstance().saveData.getNotes(selectedDate);
 		// If the save data is empty populate it with some dummy notes. This is only for debug and should be removed at launch.
 		// It also exemplifies how to use the "with" methods.
 		if(notes.isEmpty()) {
@@ -50,12 +65,63 @@ public class NotesFragment extends Fragment {
 			DataStorageManager.getInstance().save();
 		}
 
-		RecyclerView recyclerView = view.findViewById(R.id.noteRecycler);
-		recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
-
 		NoteRecyclerViewAdapter adapter = new NoteRecyclerViewAdapter(notes);
 		recyclerView.setAdapter(adapter);
+		// ==========
+		// Calendar
+		// ==========
+		dateTextView = view.findViewById(R.id.day_selector_text);
+		ImageView daySelectorLeft = view.findViewById(R.id.day_selector_left);
+		ImageView daySelectorRight = view.findViewById(R.id.day_selector_right);
 
+		dateTextView.setOnClickListener(v -> {
+			View popup = inflater.inflate(R.layout.popup_select_date,null);
+
+			final PopupWindow popupWindow = new PopupWindow(popup, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, true);
+			popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+
+
+			popup.setOnClickListener(v1 -> {
+				popupWindow.dismiss();
+			});
+			// Store the newly selected date into a temp variable and assign it to the thing we are working with only and only when
+			// we press the "select" button.
+			CalendarView calendarView = popup.findViewById(R.id.popup_calendar);
+			calendarView.setOnDateChangeListener((view1, year, month, dayOfMonth) -> {
+				tempCalendarDate = LocalDate.of(year,month + 1,dayOfMonth);
+			});
+			popup.findViewById(R.id.popup_select_button).setOnClickListener(v1 -> {
+				selectedDate = tempCalendarDate;
+				popupWindow.dismiss();
+			});
+
+			// On dismiss, update the date.
+			popupWindow.setOnDismissListener(() -> {
+				updateDate(selectedDate);
+			});
+		});
+		daySelectorLeft.setOnClickListener(v -> {
+			updateDate(selectedDate.minusDays(1));
+		});
+		daySelectorRight.setOnClickListener(v -> {
+			updateDate(selectedDate.plusDays(1));
+		});
+
+		updateDate(selectedDate);
 		return view;
+	}
+	private void updateDate(LocalDate date) {
+		selectedDate = date;
+		dateTextView.setText(selectedDate.toString());
+		updateRecyclerView();
+	}
+
+	private void updateRecyclerView() {
+		if(recyclerView == null) return;
+
+		ArrayList<Note> notes = DataStorageManager.getInstance().saveData.getNotes(selectedDate);
+		NoteRecyclerViewAdapter adapter = new NoteRecyclerViewAdapter(notes);
+		recyclerView.setAdapter(adapter);
 	}
 }
